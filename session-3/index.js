921a22861b187a6231d885ef65917d01f916c20a
l <codn3@outlook.com>
2021-11-29 20:36:12 +0000

    FutureAge calculator

--- session-3/index.js
+++ session-3/index.js
@@ -1,18 +1,14 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
-function sayHey( ) {
-    console.log("Hey") ;
-
-}
-function conversation ( ) {
-    sayHey( ) ;
-    console.log("How are you?") ;
-    console.log("Goodbye") ;
+function FutureAge(name, currentage ) {
+    
+    var FutureAge = currentage + 5 
+    return (name +  " you will be " + FutureAge) 
 }
 
-conversation( ) ;
+
+   console.log(FutureAge("Sam", 20 ) );
 
 
-
-}
+    
