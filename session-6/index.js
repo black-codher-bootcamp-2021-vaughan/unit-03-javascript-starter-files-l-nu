3f1c3c69f448557239dd5fbbff7cfb015b863f2e
l <codn3@outlook.com>
2021-12-08 19:20:37 +0000

    While Loop exercise

--- session-6/index.js
+++ session-6/index.js
@@ -1,8 +1,18 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
-
+//const randomNumber = Math.round(Math.random() * 10);
+//let counter = 0 ;
 // ****************************************************TASK 1****************************************************
 
+const randomNumber = Math.round(Math.random() * 10);
+let counter = 0 ;
+
+
+
+while (counter < 10) {
+   const randomNumber = Math.round(Math.random() * 10);
+    counter ++ ;
+    console.log('This is your random number:' + randomNumber ) ;
+}
 
 
 
