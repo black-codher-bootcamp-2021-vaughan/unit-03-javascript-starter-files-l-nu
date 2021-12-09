2b4aea95f6f6f38fb2f2a1dfa2dfc744edfba328
l <codn3@outlook.com>
2021-12-09 17:05:14 +0000

    for loop

--- session-6/index.js
+++ session-6/index.js
@@ -3,31 +3,33 @@
 //let counter = 0 ;
 // ****************************************************TASK 1****************************************************
 
-const randomNumber = Math.round(Math.random() * 10);
-let counter = 0 ;
+// const randomNumber = Math.round(Math.random() * 10);
+// let counter = 0 ;
 
 
 
-while (counter < 10) {
-   const randomNumber = Math.round(Math.random() * 10);
-    counter ++ ;
-    console.log('This is your random number:' + randomNumber ) ;
-}
-
-
-
-
-
-
-
-
-
+// while (counter < 10) {
+// //    const randomNumber = Math.round(Math.random() * 10);
+//     counter ++ ;
+//     console.log('This is your random number:' + randomNumber ) ;
+// }
 
 
 
 
 
 // ****************************************************TASK 2****************************************************
+
+
+
+console.log("Hi")
+
+for (let counter = 0 ; counter <= 10 ; counter ++) {
+    const randomNumber = Math.round(Math.random() * 10);
+     randonNumber = randomNumber + 1
+     
+     console.log("random number:" + randomNumber)
+ }
 
 
 
