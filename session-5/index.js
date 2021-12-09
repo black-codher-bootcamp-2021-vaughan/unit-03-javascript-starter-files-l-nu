b1207b086eaa9454ad7f18842dade7dc1003324d
l <codn3@outlook.com>
2021-12-09 17:07:21 +0000

    Switch statement

--- session-5/index.js
+++ session-5/index.js
@@ -8,10 +8,6 @@ function getGrade (total) {
     console.log("A")
     }
 
-    // if (total > 80) {
-    // console.log("A")
-    // }
-
     if (total> 70 && total <= 80) {
         console.log("B")
     }
@@ -61,3 +57,47 @@ getGrade(80)
 // check if your logic works, e.g. getGrade(55) should give you the grade D
 
 // ● Commit and push to GitHub
+
+
+////////////////////////////////////////////Task  2  //////////////////////////////////////////////////////
+
+function getGrade(total){
+
+switch(true) {
+    case (total > 90) :
+        console.log("A+") ;
+        break ;
+
+    case (total > 80 && total< 90) :
+        console.log("A") ;
+        break ;
+
+    case (total > 70 && total <= 80) :
+        console.log("B") ;
+        break ;
+
+    case (total > 60 && total <= 70) :
+        console.log("C") ;
+        break ;
+
+    case (total > 50 && total <= 60) :
+        console.log("D")
+        break ;
+
+    case (total > 40 && total <= 50) :
+        console.log("E")
+        break ;
+
+    case (total > 30 && total <= 40) :
+        console.log("F")
+        break ;
+
+    default:
+        console.log("Fail")
+        break ;
+}
+   
+    
+    }
+ 
+  console.log(getGrade(35))                                         
